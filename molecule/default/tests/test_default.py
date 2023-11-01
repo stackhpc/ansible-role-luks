90ad130238e48129e027ebe7a3ad804f4c9b65bb
Grzegorz Koper <grzegorzk@stackhpc.com>
2023-11-01 12:51:58 +0100

    Removing symlinks.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,1 +1,24 @@
-../../tests/test_default.py
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_key_files_exist(host):
+    f = host.file('/etc/luks-keys/dev-loop0')
+    assert f.exists
+    f = host.file('/etc/luks-keys/dev-loop1')
+    assert f.exists
+
+
+@pytest.mark.parametrize('file, content', [
+    ("/etc/crypttab", "cryptotest /dev/loop0 /etc/luks-keys/dev-loop0"),
+    ("/etc/crypttab", "crypto-test1 /dev/loop1 /etc/luks-keys/dev-loop1")
+])
+def test_crypttab(host, file, content):
+    file = host.file(file)
+    assert file.exists
+    assert file.contains(content)
